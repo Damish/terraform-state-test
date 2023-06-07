1d1f3814609e3e9a2cbbc71c7a266393018fb059
Damish <damishs88@gmail.com>
2023-06-07 17:42:09 +0530

    add destroy only one resource

--- run.sh
+++ run.sh
@@ -1,8 +1,18 @@
-cd "E:\Test Projects\aws-s3-lifecycle"
-terraform init -reconfigure
-terraform apply -auto-approve
+##step 1 - initial stage
+# cd "E:\Test Projects\aws-s3-lifecycle"
+# terraform init -reconfigure
+# terraform apply -auto-approve
 
-cd "E:\Test Projects\aws-s3-lifecycle\1"
-terraform init -reconfigure
-terraform apply -auto-approve
+##step 2 
+##Part A- destroy only required s3 bucket
+# cd "E:\Test Projects\aws-s3-lifecycle"
+# terraform init -reconfigure
+# terraform destroy -target aws_s3_bucket.example1 -auto-approve
 
+##Part B - create new s3 resources from new tf script
+# cd "E:\Test Projects\aws-s3-lifecycle\1"
+# terraform init -reconfigure
+# terraform apply -auto-approve
+
+##step 3 remove resource from terraform script manually
+## no conflict since state is updated from previous destroy step
